effab1140b6d2a59d556650796dea7b23b4a16fe
Alexandr Besarab <a.besarab@yahoo.com>
2018-12-21 10:39:30 +0300

    HW12. Edited MainStream.

--- src/MainStream.java
+++ src/MainStream.java
@@ -1,37 +1,30 @@
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
+import java.util.*;
 import java.util.concurrent.atomic.AtomicInteger;
+import java.util.stream.Collectors;
 
 public class MainStream {
     public static void main(String[] args) {
         System.out.println(minValue(new int[]{1, 2, 3, 3, 2, 9, 8, 6, 2}));
-        List<Integer> list = Arrays.asList(1, 2, 3, 3, 2, 9, 8, 6, 2);
+        List<Integer> list = Arrays.asList(2, 3, 2, 3);
         System.out.println(oddOrEven(list));
     }
 
-    private static int minValue(int[] values) {
-        AtomicInteger minValue = new AtomicInteger();
-        Arrays.stream(values)
+    private static OptionalInt minValue(int[] values) {
+        return Arrays.stream(values)
                 .distinct()
                 .sorted()
-                .forEach((p) -> minValue.set(p + minValue.get() * 10));
-        return minValue.get();
+                .reduce((p1, p2) -> p1 * 10 + p2);
     }
 
     private static List<Integer> oddOrEven(List<Integer> integers) {
-        List<Integer> list = new ArrayList<>();
-        int sum = integers.stream().mapToInt(element -> element).sum();
-        if (sum % 2 != 0) {
-            integers.stream()
-                    .filter(p -> p % 2 == 0)
-                    .forEach(list::add);
-            return list;
-        } else {
-            integers.stream()
-                    .filter(p -> p % 2 != 0)
-                    .forEach(list::add);
-            return list;
-        }
+
+//        Map<Boolean, List<Integer>> partition = integers.stream()
+//                .collect(Collectors.partitioningBy(p -> p % 2 != 0));
+//        return partition.get(integers.stream().mapToInt(element -> element).sum() % 2 == 0);
+
+        AtomicInteger sum = new AtomicInteger(0);
+        Map<Boolean, List<Integer>> partition = integers.stream().peek(p -> sum.set(sum.get() + p))
+                .collect(Collectors.partitioningBy(p -> p % 2 != 0));
+        return partition.get((sum.get() % 2) == 0);
     }
 }
